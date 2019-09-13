2821c2531c5081e8a7e5dc46fc4e02ba704e6414
woounnan <woounnan@gmail.com>
2019-09-13 23:52:19 +0900

    Add login apis in be

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors =require('cors')app.use(cors())
+const cors =require('cors')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
