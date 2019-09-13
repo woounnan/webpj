b9f837f27574b5353079f2ffabe4eabe7d22d764
woounnan <woounnan@gmail.com>
2019-09-13 23:09:31 +0900

    add Register.vue, route to reg in router.js

---
+++ front/src/views/Register.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="8">
+      <v-card>
+        <v-card-title class="cyan darken-1">
+          <span class="headline white--text">사용자 정보 등록</span>
+
+          <div class="flex-grow-1"></div>
+          <v-btn dark icon @click="sub">
+            <v-icon>check</v-icon>
+          </v-btn>
+
+          <v-btn dark icon @click="closeWindow">
+            <v-icon>cancel</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-list>
+          <v-list-item @click="">
+
+            <v-list-item-action>
+              <v-icon>person</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-text-field
+                    v-model="v_user.name"
+                    type="text"
+                    value="아이디 입력"
+                  ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>lock</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-text-field
+                    v-model="v_user.pw"
+                    type="password"
+                    value="패스워드 입력"
+                  ></v-text-field>
+            </v-list-item-content>
+          </v-list-item>
+
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-email</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>mcbeal@example.com</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-action>
+              <v-icon>mdi-map-marker</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Orlando, FL 79938</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import Vue from 'vue'
+export default {
+  data () {
+    return {
+      v_user : { 
+      name : '',
+      pw : ''
+      }
+    }
+  },props : [
+      'test'
+  ],methods : {
+      sub(){
+        console.log('call the sub function');
+        
+        var bus = new Vue()
+        bus.$on('test', 1)
+        this.closeWindow();
+      },
+      closeWindow(){
+        console.log('call the closeWindow function');
+        this.$emit('close')
+      }
+  },created () {
+  }
+}
+</script>
