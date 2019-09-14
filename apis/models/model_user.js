d90a1841a248d97842c95247f6dd6118849138d0
unknown <woounnan@gmail.com>
2019-09-14 17:08:25 +0900

    Add the function adding to user in db_utils.js

--- apis/models/model_user.js
+++ apis/models/model_user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
 	id: { type: String, required: true, unique: true, trim: true, index: true},
-	password: { type: String, required: true, unique: true, trim: true },
+	password: { type: String, required: true, trim: true },
 	key: { type: String, required: true, unique: true, default: Date.now},
 	division: { type: String, required: true, trim: true},
 	company: { type: String, required: true, trim: true}
