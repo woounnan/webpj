a9a5f014e90d4e2ff58ff39b15c1254db3a08b07
unknown <woounnan@gmail.com>
2019-09-14 16:33:34 +0900

    Add the function adding to user in db_utils.js

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -3,6 +3,7 @@ var express = require('express')
 var router = express.Router()
 var createError = require('http-errors')
 var Company = require('./models/model_company')
+var User = require('./models/model_user')
 
 router.get('/', (req, res, next) => {
 	res.send('Here is at db_utils.js')
@@ -12,8 +13,31 @@ router.get('/', (req, res, next) => {
 router.post('/addCp', (req, res, next) => {
 	var user = {}
 	user = req.body
-	res.send('name: ' + user)
-	console.log('name: ' + user.name)
+	var ret = 0
+	//find a user
+	User.findOne({id: user.id}, (e, r) =>{
+		if(e){
+			res.send({code:-1})
+			return console.error(e)
+		}
+		if(!r){
+			console.log('Can create!')
+			ret = 1
+		}
+		else{
+			console.log('Cant create T.t')
+			res.send({code: -1})
+			return 
+		}
+	})
+	if(ret == 1){
+		var users = new User(user)
+		users.save( (e, users) =>{
+			if(e) return console.error('login.js::10....occurred at users.save')
+				console.log('User created')
+		})
+	}
+	res.send({code: 1})
 })
 
 router.post('/getCp', (req, res, next) => {
