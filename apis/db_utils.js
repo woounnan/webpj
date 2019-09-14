d86f90cf85342f150dd66182e51fb3c9a91f423a
unknown <woounnan@gmail.com>
2019-09-14 13:11:50 +0900

    testing to get company in db_utils.js

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -4,6 +4,10 @@ var router = express.Router()
 var createError = require('http-errors')
 var models = require('./model')
 
+router.get('/', (req, res, next) => {
+	res.send('Here is at db_utils.js')
+	next()
+})
 router.get('/getCompanys', (req, res, next) => {
 	res.send(models)
 })
