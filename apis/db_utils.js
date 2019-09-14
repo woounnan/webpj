09a862a1592eac3e9776e1cfb7d61c636fced433
unknown <woounnan@gmail.com>
2019-09-14 13:22:47 +0900

    testing to get company in db_utils.js

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -9,7 +9,11 @@ router.get('/', (req, res, next) => {
 	next()
 })
 router.get('/getCompanys', (req, res, next) => {
-	console.log(Company.find())
+	console.log(Company.find((e, companys) => {
+		if(e) res.send('error!!: ' + e)
+		console(companys)
+		res.json(companys)
+	}))
 })
 
 module.exports = router
