a714deb777ec3059632d3361ee0df80513981ba0
woounnan <woounnan@gmail.com>
2019-09-15 10:14:26 +0900

    Testing navigation in router.js

--- front/src/router.js
+++ front/src/router.js
@@ -34,7 +34,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
       beforeEnter: (to, from, next) =>{
 
         console.log('Here is at home')
