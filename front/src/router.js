84ac5d8042a92977bb8d2aa11090792c624bfeef
woounnan <woounnan@gmail.com>
2019-09-15 10:09:17 +0900

    Testing navigation in router.js

--- front/src/router.js
+++ front/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Register from './views/Register.vue'
+import store from './store'
 
 Vue.use(Router)
 
