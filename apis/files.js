3794687ab9e0f80370d2cb03ebd70f807672adbd
woounnan <woounnan@gmail.com>
2019-09-15 19:50:17 +0900

    add file pond page

--- apis/files.js
+++ apis/files.js
@@ -2,6 +2,11 @@ var express = require('express')
 var router = express.Router()
 var createError = require('http-errors')
 
+router.get('/save', (req, res, next) => {
+	var recv = res.body
+	console.log('call fileSave on server ::get')
+	res.send(JSON.stringify(recv))
+})
 
 router.post('/save', (req, res, next) => {
 	var recv = res.body
