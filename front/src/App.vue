c03981040fd0f841d309841ab25751f9ae4bb163
woounnan <woounnan@gmail.com>
2019-09-15 14:04:42 +0900

    Add navigation bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -21,11 +21,12 @@
         </v-row>
       </v-container>
 
-  
+
 <v-navigation-drawer
       expand-on-hover
       permanent
     >
+      <!-- 사용자 정보 표시 ~~ 나중에 되면
       <template v-slot:prepend>
         <v-list>
           <v-list-item>
@@ -33,7 +34,6 @@
               <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
             </v-list-item-avatar>
           </v-list-item>
-
           <v-list-item
             link
             two-line
@@ -46,11 +46,12 @@
               <v-icon>mdi-menu-down</v-icon>
             </v-list-item-action>
           </v-list-item>
+
         </v-list>
       </template>
 
       <v-divider></v-divider>
-
+          -->
       <v-list
         nav
         dense
@@ -60,18 +61,6 @@
             <v-icon>mdi-folder</v-icon>
           </v-list-item-icon>
           <v-list-item-title>My Files</v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-icon>
-            <v-icon>mdi-account-multiple</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Shared with me</v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-icon>
-            <v-icon>mdi-star</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Starred</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -120,7 +109,21 @@ Vue.use(Vuetify)
 export default {
   name: 'App',
   data: () => ({
-    
+    items: [
+        {
+          icon: 'home',
+          title: 'Home',
+          to: {
+            path: '/'
+          }
+        },
+        {
+          icon: 'view',
+          title: 'View',
+          to: {
+            path: '/about'
+          }
+        }
   }),
   methods: {
     myNext(path){
