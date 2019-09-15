05c3ca69e84bbbdd39da796f7f36d47d173a001f
woounnan <woounnan@gmail.com>
2019-09-15 10:40:27 +0900

    Testing navigation in router.js

--- front/src/router.js
+++ front/src/router.js
@@ -15,7 +15,7 @@ const requireAuth = (to, from, next) =>{
         console.log(store.state.tk)
 
         console.log(!store.state.tk)
-        console.log(JSON.stringify(next.url))
+        console.log(JSON.stringify(next))
 }
 
 export default new Router({
