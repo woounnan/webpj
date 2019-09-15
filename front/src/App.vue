6ce08b7d48a83d411b43e87f685f8f8281dba99b
woounnan <woounnan@gmail.com>
2019-09-15 14:24:23 +0900

    Add navigation bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <v-app>
-    <v-container class="grey lighten-5">
-    <v-row no-gutters>
-    <v-app-bar app>
-    <v-toolbar-title class="headline text-uppercase">
+   <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-toolbar-title class="headline text-uppercase">
       <v-btn
       text
       @click="$router.push('/')"
@@ -31,72 +57,17 @@
       </v-btn>
     </div>
   </v-app-bar>
-</v-row>
-        <v-row no-gutters>
-            <v-col :key="1">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                <v-navigation-drawer
-                  expand-on-hover
-                  permanent
-                >
-                  <!-- 사용자 정보 표시 ~~ 나중에 되면
-                  <template v-slot:prepend>
-                    <v-list>
-                      <v-list-item>
-                        <v-list-item-avatar>
-                          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-                        </v-list-item-avatar>
-                      </v-list-item>
-                      <v-list-item
-                        link
-                        two-line
-                      >
-                        <v-list-item-content>
-                          <v-list-item-title class="title">Sandra Adams</v-list-item-title>
-                          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-                        </v-list-item-content>
-                        <v-list-item-action>
-                          <v-icon>mdi-menu-down</v-icon>
-                        </v-list-item-action>
-                      </v-list-item>
+    </v-app-bar>
 
-                    </v-list>
-                  </template>
-
-                  <v-divider></v-divider>
-                      -->
-                  <v-list
-                    nav
-                    dense
-                  >
-                    <v-list-item link>
-                      <v-list-item-icon>
-                        <v-icon>mdi-folder</v-icon>
-                      </v-list-item-icon>
-                      <v-list-item-title>My Files</v-list-item-title>
-                    </v-list-item>
-                  </v-list>
-                </v-navigation-drawer>
-
-              </v-card>
-            </v-col>
-             <v-col :key="2">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                <v-content>
-                <router-view/>
-              </v-content>
-              </v-card>
-            </v-col>
-        </v-row>
-      </v-container>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
