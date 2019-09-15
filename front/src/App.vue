649b0421b2c646f3af45be5b6b50c680120a20d5
woounnan <woounnan@gmail.com>
2019-09-15 14:10:22 +0900

    Add navigation bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -37,7 +37,49 @@
                 outlined
                 tile
               >
-                Column1
+                <v-navigation-drawer
+                  expand-on-hover
+                  permanent
+                >
+                  <!-- 사용자 정보 표시 ~~ 나중에 되면
+                  <template v-slot:prepend>
+                    <v-list>
+                      <v-list-item>
+                        <v-list-item-avatar>
+                          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                        </v-list-item-avatar>
+                      </v-list-item>
+                      <v-list-item
+                        link
+                        two-line
+                      >
+                        <v-list-item-content>
+                          <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+                          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                          <v-icon>mdi-menu-down</v-icon>
+                        </v-list-item-action>
+                      </v-list-item>
+
+                    </v-list>
+                  </template>
+
+                  <v-divider></v-divider>
+                      -->
+                  <v-list
+                    nav
+                    dense
+                  >
+                    <v-list-item link>
+                      <v-list-item-icon>
+                        <v-icon>mdi-folder</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-title>My Files</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-navigation-drawer>
+
               </v-card>
             </v-col>
              <v-col :key="2">
@@ -46,60 +88,13 @@
                 outlined
                 tile
               >
-                Column2
+                <v-content>
+                <router-view/>
+              </v-content>
               </v-card>
             </v-col>
         </v-row>
       </v-container>
-
-
-<v-navigation-drawer
-      expand-on-hover
-      permanent
-    >
-      <!-- 사용자 정보 표시 ~~ 나중에 되면
-      <template v-slot:prepend>
-        <v-list>
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-            </v-list-item-avatar>
-          </v-list-item>
-          <v-list-item
-            link
-            two-line
-          >
-            <v-list-item-content>
-              <v-list-item-title class="title">Sandra Adams</v-list-item-title>
-              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-icon>mdi-menu-down</v-icon>
-            </v-list-item-action>
-          </v-list-item>
-
-        </v-list>
-      </template>
-
-      <v-divider></v-divider>
-          -->
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item link>
-          <v-list-item-icon>
-            <v-icon>mdi-folder</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>My Files</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-
-      <v-content>
-      <router-view/>
-    </v-content>
   </v-app>
 </template>
 
