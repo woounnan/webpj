b7967c8d67b57f9d8a3f72ea91f81fca41a7f5a7
woounnan <woounnan@gmail.com>
2019-09-15 14:30:56 +0900

    Add navigation bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,20 +5,16 @@
       app
     >
       <v-list dense>
-        <v-list-item @click="">
+        <v-list-item 
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          >
           <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
+            <v-icon v-html="item.icon"></v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="">
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
