700c9168239472b798bc316f9ad3711938c61903
woounnan <woounnan@gmail.com>
2019-09-15 14:14:51 +0900

    Add navigation bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,45 +1,6 @@
 <template>
   <v-app>
-    <v-container class="grey lighten-5">
-    <v-row no-gutters>
-    <v-app-bar app>
-    <v-toolbar-title class="headline text-uppercase">
-      <v-btn
-      text
-      @click="$router.push('/')"
-      >
-        <span>WorkTalk</span><v-icon>chat_bubble_outline</v-icon>
-      </v-btn>
-    </v-toolbar-title>
-    <v-spacer></v-spacer>
-    <div v-if="!$store.state.tk">
-      <v-btn
-        text
-        @click="myNext('/login')"
-      >
-      <span class="mr-2">로그인</span>
-      <v-icon>done</v-icon>
-      </v-btn>
-    </div>
-   <div v-else name='test'>
-      <v-btn
-        text
-        @click="logOut"
-      >
-      <span class="mr-2">로그아웃</span>
-      <v-icon>close</v-icon>
-      </v-btn>
-    </div>
-  </v-app-bar>
-</v-row>
-        <v-row no-gutters>
-            <v-col :key="1">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                <v-navigation-drawer
+ <v-navigation-drawer
                   expand-on-hover
                   permanent
                 >
@@ -81,22 +42,37 @@
                     </v-list-item>
                   </v-list>
                 </v-navigation-drawer>
-
-              </v-card>
-            </v-col>
-             <v-col :key="2">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
+    <v-toolbar-title class="headline text-uppercase">
+      <v-btn
+      text
+      @click="$router.push('/')"
+      >
+        <span>WorkTalk</span><v-icon>chat_bubble_outline</v-icon>
+      </v-btn>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <div v-if="!$store.state.tk">
+      <v-btn
+        text
+        @click="myNext('/login')"
+      >
+      <span class="mr-2">로그인</span>
+      <v-icon>done</v-icon>
+      </v-btn>
+    </div>
+   <div v-else name='test'>
+      <v-btn
+        text
+        @click="logOut"
+      >
+      <span class="mr-2">로그아웃</span>
+      <v-icon>close</v-icon>
+      </v-btn>
+    </div>
+          
                 <v-content>
                 <router-view/>
               </v-content>
-              </v-card>
-            </v-col>
-        </v-row>
-      </v-container>
   </v-app>
 </template>
 
