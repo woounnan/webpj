bf47fff79ae1c41e1eee8a25fc3324e979a04de1
woounnan <woounnan@gmail.com>
2019-09-18 23:32:16 +0900

    test open subView

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -23,23 +23,30 @@
       </v-btn>
     </v-toolbar>
     <v-list>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        @click=""
-      >
-        <v-list-item-icon>
-          <v-icon v-if="item.icon" color="pink">mdi-star</v-icon>
-        </v-list-item-icon>
+      <template v-slot:activator="{ on }">
+        <v-list-item
+          v-for="item in list_user"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
 
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item-content>
-
-        <v-list-item-avatar>
-          <v-img :src="item.avatar"></v-img>
-        </v-list-item-avatar>
-      </v-list-item>
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+      </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, i) in subView"
+                  :key="i"
+                  @click="() => {}"
+                >
+                  <v-list-item-title>{{ item }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
     </v-list>
   </v-card>
 </template>
@@ -48,12 +55,16 @@
   export default {
     data () {
       return {
-        items: [
+        list_user: [
           { icon: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
           { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
           { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
           { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
         ],
+        subView: [
+          '대화하기',
+          '업무확인'
+        ]
       }
     },
   }
