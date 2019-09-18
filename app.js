2035923a418b83108da9fd9e35e3b1309b10dfca
woounnan <woounnan@gmail.com>
2019-09-18 23:06:58 +0900

    test recieve messsage

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ io.on('connection', function(socket){
 		console.log('from client: ' + data)
 	})
 	socket.interval = setInterval(() => {
+		console.log('send msg to client')
 		socket.emit('msg', 'Hi ! im server')
 	}, 3000)
 })
