9adc779034f2ef39d0d0a21ee64653503746bc28
woounnan <woounnan@gmail.com>
2019-09-19 19:44:56 +0900

    fix recieve msg from server

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apisRouter = require('./apis/index');
 
-
+var app = express();
 app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
