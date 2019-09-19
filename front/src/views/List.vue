c413e950af87060d40fc9a433093fd40723f6e0f
woounnan <woounnan@gmail.com>
2019-09-19 22:45:54 +0900

    add click bar

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -10,61 +10,12 @@
       style="max-width: 600px"
     >
       <template v-slot:activator="{ on }">
-       <v-toolbar-title>New Chat</v-toolbar-title>
-
-      <div class="flex-grow-1"></div>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-list subheader>
-      <v-subheader>Recent chat</v-subheader>
-
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        @click=""
-      >
-        <v-list-item-avatar>
-          <v-img :src="item.avatar"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item-content>
-
-        <v-list-item-icon>
-          <v-icon :color="item.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
-    <v-list subheader>
-      <v-subheader>Previous chats</v-subheader>
-
-      <v-list-item
-        v-for="item in items2"
-        :key="item.title"
-        @click=""
-      >
-        <v-list-item-avatar>
-          <v-img :src="item.avatar"></v-img>
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+        <Users @click="on"/>
       </template>
 
       <v-list>
         <v-list-item
-          v-for="(item, index) in sub"
+          v-for="(item, index) in items"
           :key="index"
           @click=""
         >
@@ -76,22 +27,17 @@
 </template>
 
 <script>
- export default {
+import Users from './Users.vue'
+export default {
+    components: (){
+      Users
+    },
     data: () => ({
-      items: [
-        { active: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-        { active: true, title: 'Ranee Carlson', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-        { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-        { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
-      ],
-      items2: [
-        { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg' },
-      ],
       sub: [
         '대화하기',
         '작업 확인',
         '상태'
       ]
     }),
-  }
+}
 </script>
