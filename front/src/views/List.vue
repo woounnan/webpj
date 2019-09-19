b3d21aecb9935b38bb4fe718df740572e86524f5
woounnan <woounnan@gmail.com>
2019-09-19 22:32:31 +0900

    add click bar

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -1,80 +1,98 @@
 <template>
-  
   <v-card
     max-width="500"
     class="mx-auto"
   >
     <v-toolbar
-      color="indigo"
+      color="deep-purple accent-4"
       dark
     >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>목록</v-toolbar-title>
+      <v-toolbar-title>New Chat</v-toolbar-title>
 
       <div class="flex-grow-1"></div>
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-toolbar>
-    <v-list>
-        <v-list-item
-          v-for="item in list_user"
-          :key="item.title"
-        >
-          <v-menu offset-y>
-            <template v-slot:activator="{ on }">
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.title"></v-list-item-title>
-                </v-list-item-content>
 
-                <v-list-item-avatar>
-                  <v-img :src="item.avatar"></v-img>
-                </v-list-item-avatar>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(item, index) in items"
-                :key="index"
-                @click=""
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-list-item>
+    <v-list subheader>
+      <v-subheader>Recent chat</v-subheader>
+
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        @click=""
+      >
+        <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-icon>
+            <v-icon :color="item.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+        </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+              @click=""
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        </v-list-item-icon>
+      </v-list-item>
     </v-list>
 
+    <v-divider></v-divider>
+
+    <v-list subheader>
+      <v-subheader>Previous chats</v-subheader>
+
+      <v-list-item
+        v-for="item in sub"
+        :key="item"
+        @click=""
+      >
+        <v-list-item-avatar>
+          <v-img :src="item.avatar"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        items: {
-          title: [
-          'Ganeey',
-          'Morning',
-          'Parari'
-          ]
-        },
-        list_user: [
-          { icon: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
-          { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-          { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-          { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
-        ],
-        subView: [
-          '대화하기',
-          '업무확인'
-        ]
-      }
-    },
+ export default {
+    data: () => ({
+      items: [
+        { active: true, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
+        { active: true, title: 'Ranee Carlson', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
+        { title: 'Cindy Baker', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
+        { title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
+      ],
+      items2: [
+        { title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg' },
+      ],
+      sub: [
+        '대화하기',
+        '작업 확인',
+        '상태'
+      ]
+    }),
   }
 </script>
 
