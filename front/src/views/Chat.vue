56486a9baf968194c4b3a4643a578fae00a74cba
woounnan <woounnan@gmail.com>
2019-09-21 15:01:56 +0900

    adjust grid

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -1,8 +1,32 @@
 <template>
+	 <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-row
+          :align="alignment"
+          :justify="justify"
+          class="grey lighten-5"
+          style="height: 300px;"
+        >
+          <v-card
+            v-for="n in 3"
+            :key="n"
+            class="ma-3 pa-6"
+            outlined
+            tile
+          >
+            Column
+          </v-card>
+        </v-row>
+      </v-col>
+  </v-row>
+</v-container>
+	<!--
 	<div>
 	<basic-vue-chat />
 <List />
 <Bar />
+-->
 </div>
 
 </template>
