a4e003972b10082616e8d58aea38ed5a9b2a1d0e
woounnan <woounnan@gmail.com>
2019-09-21 19:54:27 +0900

    test toolbar

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -46,6 +46,21 @@
           <v-icon :color="me.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
         </v-list-item-icon>
       </v-list-item>
+      <v-list-item
+        v-for="item, idx in users"
+        @click="setThis(idx)">
+        <v-list-item-avatar>
+          <v-img :src="item.avatar"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-icon>
+          <v-icon :color="item.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+        </v-list-item-icon>
+      </v-list-item>
     </v-list>
   </v-card>    
       </template>
