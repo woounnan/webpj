2cc0800c8cd2f2defa8d0539dfdcacfb8878542e
woounnan <woounnan@gmail.com>
2019-09-22 13:13:11 +0900

    modify files

--- front/src/views/Bar.vue
+++ front/src/views/Bar.vue
@@ -1,8 +1,9 @@
 <template>
 	<v-system-bar dark color="indigo darken-2">
-	  <v-icon>mdi-wifi-strength-4</v-icon>
-	  <v-icon>mdi-signal-cellular-outline</v-icon>
-	  <v-icon>mdi-battery</v-icon>
-	  <span>12:30</span>
+		<div class="flex-grow-1"></div>
+		<v-icon>mdi-wifi-strength-4</v-icon>
+		<v-icon>mdi-signal-cellular-outline</v-icon>
+		<v-icon>mdi-battery</v-icon>
+		<span>12:30</span>
 	</v-system-bar>
 </template>
