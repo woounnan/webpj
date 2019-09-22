32ce124d1009f672be41f3f81428bfb4fce9aaf0
woounnan <woounnan@gmail.com>
2019-09-22 13:20:45 +0900

    modify a bar

--- front/src/views/Bar.vue
+++ front/src/views/Bar.vue
@@ -1,9 +1,17 @@
 <template>
 	<v-system-bar dark color="indigo darken-2">
-		<v-btn @click="" height="20px">
-			<v-icon>mdi-message</v-icon>
-			양세형
-		</v-btn>
+		<v-menu
+	      v-model="showMenu"
+	      absolute
+	      offset-y
+	    >
+		    <template v-slot:activator="{ on }">
+				<v-btn v-on="on" height="20px">
+					<v-icon>mdi-message</v-icon>
+					양세형
+				</v-btn>
+			</template>
+			<Chat />
 		<div class="flex-grow-1"></div>
 		<v-icon>mdi-wifi-strength-4</v-icon>
 		<v-icon>mdi-signal-cellular-outline</v-icon>
@@ -11,3 +19,17 @@
 		<span>12:30</span>
 	</v-system-bar>
 </template>
+
+<script>
+	import Chat from '../../node_modules/basic-vue-chat/src/components/basic-vue-chat/BasicVueChat.vue'
+  	export default {
+	    components: {
+	      Chat
+	    },
+	    data: () => {
+	    	return {
+
+	    	}
+	  	}
+	}
+</script>
