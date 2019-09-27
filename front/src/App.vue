ae8a9d7e69535fd9928fb38e01241d96596dbfa2
woounnan <woounnan@gmail.com>
2019-09-27 23:49:13 +0900

    test socket

--- front/src/App.vue
+++ front/src/App.vue
@@ -33,6 +33,7 @@
       >
         <span>ArmyTalk</span><v-icon>warning</v-icon>
       </v-btn>
+      <v-btn @click="showSt($store.state.tk)"> test </v-btn>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <div v-if="!$store.state.tk">
@@ -97,6 +98,9 @@ export default {
       this.$store.commit('delToken')
       localStorage.removeItem('tk')
       location.href='/'
+    },
+    showSt(st){
+      console.log(st)
     }
   }
 };
