1bc46940d6452337d58c57b4b8799f9184d71381
woounnan <woounnan@gmail.com>
2019-09-27 23:19:57 +0900

    test socket

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -119,6 +119,7 @@ import Users from './Users.vue'
     },
     mounted(){
       var im = this.$store.getters.getUser
+      console.log('Listvue:: ',im)
       this.me.pos = im.pos
       this.me.st = im.st
       this.me.img = im.img
