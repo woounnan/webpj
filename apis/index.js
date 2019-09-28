7e441b6864d6c88a31b246ee8ae2eef4fc869d70
woounnan <woounnan@gmail.com>
2019-09-28 18:17:12 +0900

    test msg

--- apis/index.js
+++ apis/index.js
@@ -27,7 +27,7 @@ io.on('connection', function(socket){
 	*/
 	socket.on('msg', (data) =>{
 		console.log('recv msg, so send msg')
-		socket.emit('msg', data)
+		socket.broadcast.emit('msg', data)
 	})
 })
 
