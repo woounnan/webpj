640b357fc362ab57778b0b9a4cbe79edb8e5a90e
woounnan <woounnan@gmail.com>
2019-09-29 12:38:57 +0900

    compare list with division in List.vue

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -49,25 +49,26 @@
       <v-divider></v-divider>
 
       <div v-for="(cp, idx) in $store.state.companys">
-      <v-subheader>{{cp.name}}</v-subheader>
-      <v-list-item
-        v-for="other, idx in $store.state.others"
-        @click="setThis(idx)"
-        v-on="on">
-        <div v-if="other.division === cp.name">
-          <v-list-item-avatar>
-            <v-img :src="other.image"></v-img>
-          </v-list-item-avatar>
+        <v-subheader>{{cp.name}}</v-subheader>
+        <v-list-item
+          v-for="other, idx in $store.state.others"
+          @click="setThis(idx)"
+          v-on="on">
+          <div v-if="other.division === cp.name">
+            <v-list-item-avatar>
+              <v-img :src="other.image"></v-img>
+            </v-list-item-avatar>
 
-          <v-list-item-content>
-            <v-list-item-title v-text="other.position"></v-list-item-title>
-          </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-title v-text="other.position"></v-list-item-title>
+            </v-list-item-content>
 
-          <v-list-item-icon>
-            <v-icon :color="other.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
-          </v-list-item-icon>
-        </div>
-      </v-list-item>
+            <v-list-item-icon>
+              <v-icon :color="other.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+            </v-list-item-icon>
+          </div>
+        </v-list-item>
+      </div>
     </v-list>
   </v-card>    
       </template>
