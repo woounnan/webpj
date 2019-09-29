a91e0d26a871f6856c322a047e22ccbc76985c57
woounnan <woounnan@gmail.com>
2019-09-29 23:23:36 +0900

    recv msg in BasicChat.vue

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -83,7 +83,7 @@
           :key="idx"
           @click="subCall(idx)"
         >
-            <v-list-item-title @click="checkComu(item.title)">{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-dialog>
         </v-list-item>
       </v-list>
