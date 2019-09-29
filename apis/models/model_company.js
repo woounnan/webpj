79111fd6fd8a1562fe288abcabdda72cfdd7fd30
woounnan <woounnan@gmail.com>
2019-09-29 16:21:46 +0900

    create list_company

--- apis/models/model_company.js
+++ apis/models/model_company.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const con = require('../../conMongo')
 
 const companySchema = new mongoose.Schema({
 	name: { type: String, required: true, unique: true, trim: true, index: true},
