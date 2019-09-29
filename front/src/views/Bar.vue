46501c23913c0c4d0b7a08e072ebb13682fca064
woounnan <woounnan@gmail.com>
2019-09-29 13:28:01 +0900

    compare list with division in List.vue

--- front/src/views/Bar.vue
+++ front/src/views/Bar.vue
@@ -15,7 +15,7 @@
 						>
 							<v-icon>mdi-message</v-icon>
 							{{x}}&nbsp;
-							<v-icon v-on="" @click="closeRoom(idx)">close</v-icon>
+							<v-icon @click="closeRoom(idx)">close</v-icon>
 						</v-btn>
 						&nbsp;
 					</div>
