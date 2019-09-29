7df7feb257c5311ebf282331ab6afea60dcdf9b2
woounnan <woounnan@gmail.com>
2019-09-29 16:21:01 +0900

    create list_company

--- apis/models/model_company.js
+++ apis/models/model_company.js
@@ -10,28 +10,5 @@ const companySchema = new mongoose.Schema({
 var Company = mongoose.model('Company', companySchema, 'list_company')
 
 
-var cp = {
-	name : '',
-	division : ['체계운용과', '계획운용과', '인사과', '군수과', '본부중대', '지원1중대', '지원2중대'],
-	user: []
-}
-
-mongoose.connection.db.listCollections({name: 'list_company'}, (e, collection) =>{
-	if(!collection){
-		list_add = ['2div', '21div', '12div']
-		list_add.forEach(x => {
-			cp.name = x
-
-			var company = new Company(cp)
-			company.save( (e, cp) =>{
-				if(cp)
-					console.log('save data in collection of list_company')
-				else
-					console.error('save company in model_company.js:::', e)
-			})
-		})
-	}
-})
-
 
 module.exports = Company
