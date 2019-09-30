404e6fb6263f5a9f50d346854138fbb82f246f9b
woounnan <woounnan@gmail.com>
2019-09-30 22:46:48 +0900

    make preview.vue

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -11,7 +11,7 @@
 		<v-col 
 			cols="3">
 			<v-row row wrap>
-				<List @click="testServ" />
+				<List />
 			</v-row>
 		</v-col>
 	</v-row>
--- front/src/views/Preview.vue
+++ front/src/views/Preview.vue
@@ -1,39 +1,40 @@
 <template>
-	{{test}}
-	<v-row>
-				<v-col cols="5">
-					<v-card>
-						111
-					</v-card>
-				</v-col>
-				<v-col cols="5">
-					<v-card>
-						111
-					</v-card>
-				</v-col>
-				<v-col cols="5">
-					<v-card>
-						111
-					</v-card>
-				</v-col>
-			</v-row>
-			<v-row row wrap>
-				<v-col cols="5">
-					<v-card>
-						222
-					</v-card>
-				</v-col>
-				<v-col cols="4">
-					<v-card>
-						333
-					</v-card>
-				</v-col>
-				<v-col cols="4">
-					<v-card>
-						555
-					</v-card>
-				</v-col>
-			</v-row>
+	<v-container>
+		<v-row>
+			<v-col cols="5">
+				<v-card>
+					{{test}}
+				</v-card>
+			</v-col>
+			<v-col cols="5">
+				<v-card>
+					111
+				</v-card>
+			</v-col>
+			<v-col cols="5">
+				<v-card>
+					111
+				</v-card>
+			</v-col>
+		</v-row>
+		<v-row row wrap>
+			<v-col cols="5">
+				<v-card>
+					222
+				</v-card>
+			</v-col>
+			<v-col cols="4">
+				<v-card>
+					333
+				</v-card>
+			</v-col>
+			<v-col cols="4">
+				<v-card>
+					555
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
