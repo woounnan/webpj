9d7d8bda3b829dd3d537a705bf000750b409c1e7
woounnan <woounnan@gmail.com>
2019-09-30 19:37:35 +0900

    test chat

--- front/src/views/List.vue
+++ front/src/views/List.vue
@@ -73,8 +73,14 @@
                   </v-list-item-content>
 
                   <v-list-item-icon>
-                    <v-icon :color="other.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+                  <v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
                   </v-list-item-icon>
+                  <v-list-item-icon>
+                    <v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">work</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-icon>
+                  <v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">notification_important</v-icon>
+                </v-list-item-icon>
             </v-list-item>
         </div>
         <v-divider></v-divider>
