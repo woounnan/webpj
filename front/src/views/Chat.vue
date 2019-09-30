47972ba00cca7437911d4ccd96fdc6a283464dbf
woounnan <woounnan@gmail.com>
2019-09-30 22:44:53 +0900

    make preview.vue

--- front/src/views/Chat.vue
+++ front/src/views/Chat.vue
@@ -11,7 +11,7 @@
 		<v-col 
 			cols="3">
 			<v-row row wrap>
-				<List />
+				<List @click="testServ" />
 			</v-row>
 		</v-col>
 	</v-row>
@@ -26,7 +26,7 @@
 import Chat from '../../node_modules/basic-vue-chat/src/components/basic-vue-chat/BasicVueChat.vue'
 import Bar from './Bar.vue'
 import List from './List.vue'
-import Preview from '.Preview.vue'
+import Preview from './Preview.vue'
 import io from 'socket.io-client'
 export default{
 	name: 'App',
--- front/src/views/Preview.vue
+++ front/src/views/Preview.vue
@@ -1,5 +1,39 @@
 <template>
 	{{test}}
+	<v-row>
+				<v-col cols="5">
+					<v-card>
+						111
+					</v-card>
+				</v-col>
+				<v-col cols="5">
+					<v-card>
+						111
+					</v-card>
+				</v-col>
+				<v-col cols="5">
+					<v-card>
+						111
+					</v-card>
+				</v-col>
+			</v-row>
+			<v-row row wrap>
+				<v-col cols="5">
+					<v-card>
+						222
+					</v-card>
+				</v-col>
+				<v-col cols="4">
+					<v-card>
+						333
+					</v-card>
+				</v-col>
+				<v-col cols="4">
+					<v-card>
+						555
+					</v-card>
+				</v-col>
+			</v-row>
 </template>
 
 <script>
