5a33ddafb9076924ed81bf77c6d4d345c9f01156
woounnan <woounnan@gmail.com>
2019-10-01 09:46:45 +0900

    test getChat

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -47,8 +47,7 @@ router.post('/getChat', (req, res, next) => {
 	console.log('getChat::::srcId :', srcId)
 	console.log('getChat::::to :', to)
 	User.findOne({id: srcId}, (e, r) => {
-		console.log(r.length)
-		if(r.length > 0 ){
+		if(!e){
 			console.log('getChat::successed')
 			r.comu.forEach(x =>{
 				if(x.with === to){
