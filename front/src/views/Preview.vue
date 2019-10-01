60c3d3135d3def55f3206b9b33dd65c12caee9fd
woounnan <woounnan@gmail.com>
2019-10-01 11:20:14 +0900

    test getChat

--- front/src/views/Preview.vue
+++ front/src/views/Preview.vue
@@ -1,16 +1,16 @@
 <template>
 	<v-container>
 		<v-row>
-			<v-col cols="5">
-				<v-card
-			    max-width="500"
-			    class="mx-auto"
-			  	>
+			<v-col cols="6">
 		<!--
 			<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">work</v-icon>
           	<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">notification_important</v-icon> 
           	<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
         -->
+				<v-card
+			    max-width="500"
+			    class="mx-auto"
+			  	>
 				    <v-card-title color="skyblue">
 				    	처리할일
 				    	<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">work</v-icon>
@@ -21,15 +21,35 @@
 				    </v-card-actions>
 			  	</v-card>
 			</v-col>
-			<v-col cols="5">
-				<v-card>
-					111
-				</v-card>
+			<v-col cols="6">
+				<v-card
+			    max-width="500"
+			    class="mx-auto"
+			  	>
+				    <v-card-title color="skyblue">
+				    	요청한일
+				    	<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">work</v-icon>
+				    </v-card-title>
+				    <v-card-text>I'm card text</v-card-text>
+				    <v-card-actions>
+				      <v-btn text>더보기</v-btn>
+				    </v-card-actions>
+			  	</v-card>
 			</v-col>
-			<v-col cols="5">
-				<v-card>
-					111
-				</v-card>
+			<v-col cols="12">
+				<v-card
+			    max-width="500"
+			    class="mx-auto"
+			  	>
+				    <v-card-title color="skyblue">
+				    	알림
+				    	<v-icon :color="$store.state.user.active ? 'deep-purple accent-4' : 'grey'">work</v-icon>
+				    </v-card-title>
+				    <v-card-text>I'm card text</v-card-text>
+				    <v-card-actions>
+				      <v-btn text>더보기</v-btn>
+				    </v-card-actions>
+			  	</v-card>
 			</v-col>
 		</v-row>
 		<v-row row wrap>
