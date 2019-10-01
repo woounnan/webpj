8d81b7a87ca1e694375ad2400f94ab37b987a51c
woounnan <woounnan@gmail.com>
2019-10-01 13:07:52 +0900

    modify Preview

--- front/src/views/View.vue
+++ front/src/views/View.vue
@@ -1,16 +1,45 @@
 <template>
-	<v-container>
-		<v-row>
-		</v-row>
-	</v-container>
-	<ViewParts />
+	<v-card color="basil">
+    <v-card-title class="text-center justify-center py-6">
+      <h1 class="font-weight-bold display-3 basil--text">BASiL</h1>
+    </v-card-title>
+
+    <v-tabs
+      v-model="tab"
+      background-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="item in items"
+        :key="item"
+      >
+        <v-card flat color="basil">
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 <script>
 	import ViewParts from './ViewParts.vue'
 	export default{
 		data(){
 			return {
-
+				tab: null,
+		        items: [
+		          'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
+		        ],
+		        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 			}
 		}
 	}
