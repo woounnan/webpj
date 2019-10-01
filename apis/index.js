9f10939aa95a017f284518cfe86e30fcecdafd97
woounnan <woounnan@gmail.com>
2019-10-01 14:51:13 +0900

    add save message in index.js

--- apis/index.js
+++ apis/index.js
@@ -23,6 +23,7 @@ var getUser = function (to, from, newConvs){
 		}
 		var idx = -1
 		var i = 0
+		console.log('r.comu: ', r.comu)
 		for(x in r.comu){
 			console.log('x.with: ', x.with)
 			if(x.with === to){
