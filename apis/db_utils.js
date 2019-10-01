8172773c40834ce9bcd0a5e0576982168e0ed911
woounnan <woounnan@gmail.com>
2019-10-01 23:08:02 +0900

    verify filenames in db_utils.js

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -18,8 +18,8 @@ router.post('/save', upload.single('bin'), function (req, res, next) {
   //console.log(req)
   var realName = req.file.originalname
   var saveName = req.file.filename
-  console.log('real: ', realName)
-  console.log('save: ', saveName)
+  res.send({file_save: saveName})
+  res.send({file_real: realName})
   res.status(204).send()
   // req.body will hold the text fields, if there were any
 })
