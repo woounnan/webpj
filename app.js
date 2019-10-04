40e6477510c169093e6efcc54a5f5d91b1cbb600
woounnan <woounnan@gmail.com>
2019-10-04 20:15:30 +0900

    test download link

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.get('/public', (req, res, next) =>{
-	console.log('public!!!!!!!!!!!!!')
+	console.log('public!!!!!!!!!!!!!', path.join(__dirname)
 })
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
