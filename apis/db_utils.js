5e28f03bfaf039c8bec692d93b5ebcb87f04cfdf
woounnan <woounnan@gmail.com>
2019-10-04 23:32:41 +0900

    make message sended to multiple receivers

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -23,7 +23,8 @@ router.post('/work', upload.single('bin'), function (req, res, next) {
 		endDate : req.body.endDate,
 		notice : req.body.notice,
 	}
-	console.log(req.body.selects)	res.status(200).send({works: works})
+	console.log(works.selects)	
+	res.status(200).send({works: works})
   // req.body will hold the text fields, if there were any
 })
 
