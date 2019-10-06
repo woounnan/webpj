bbdc6f1aa920a934d53febb8a440d3885d6d9963
woounnan <woounnan@gmail.com>
2019-10-06 19:08:34 +0900

    test

--- front/src/views/View.vue
+++ front/src/views/View.vue
@@ -56,7 +56,7 @@
     },
     methods: {
       setCur(idx){
-        this.cur = String(parseInt(idx/2))
+        this.cur = parseInt(idx/2) === 0 ? 'work' : 'notice'
       },
       getWorks(){
         axios.post('http://webhacker.xyz:8000/apis/db/getWorks', {id: this.$store.getters.getUser.id})
