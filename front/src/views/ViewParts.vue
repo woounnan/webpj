c89bd927722b5fa5578805341ad48a1f7107d32c
woounnan <woounnan@gmail.com>
2019-10-06 18:26:43 +0900

    test

--- front/src/views/ViewParts.vue
+++ front/src/views/ViewParts.vue
@@ -1,144 +1,69 @@
 <template>
-	<v-row>
-		<v-col>
-    <v-card
-        max-width="500"
-        class="mx-auto"
-        flat color="basil"
+  <v-card
+    max-width="500"
+    class="mx-auto"
+  >
+    <v-toolbar
+      color="pink"
+      dark
     >
-	 	<v-toolbar
-	 	@click="showTitle"
-	      	:color="setThis === 0 ? 'purple lighten-3' : 'blue lighten-2'"
-	     	dark
-	    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-	      <v-toolbar-title >{{titles[setThis]}}</v-toolbar-title>
+      <v-toolbar-title>Inbox</v-toolbar-title>
 
-	      <div class="flex-grow-1"></div>
+      <div class="flex-grow-1"></div>
 
-	      <v-btn icon>
-	        <v-icon>mdi-magnify</v-icon>
-	      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
 
-	      <v-btn icon>
-	        <v-icon>mdi-checkbox-marked-circle</v-icon>
-	      </v-btn>
-	    </v-toolbar>
-	    <v-list two-line>
+      <v-btn icon>
+        <v-icon>mdi-checkbox-marked-circle</v-icon>
+      </v-btn>
+    </v-toolbar>
 
-	      <v-list-item-group
-	        v-model="selected"
-	        multiple
-	        active-class="pink--text"
-	      >
-	        <template v-for="(item, index) in items">
-	          <v-list-item :key="item.title">
-	            <template v-slot:default="{ active, toggle }">
-	              <v-list-item-content>
-	                <v-list-item-title v-text="item.title"></v-list-item-title>
-	                <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
-	                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
-	              </v-list-item-content>
+    <v-list two-line>
+      <v-list-item-group
+        v-model="selected"
+        multiple
+        active-class="pink--text"
+      >
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.title">
+            <template v-slot:default="{ active, toggle }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+              </v-list-item-content>
 
-	              <v-list-item-action>
-	                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
-	                <v-icon
-	                  v-if="!active"
-	                  color="grey lighten-1"
-	                >
-	                  star_border
-	                </v-icon>
+              <v-list-item-action>
+                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
+                <v-icon
+                  v-if="!active"
+                  color="grey lighten-1"
+                >
+                  star_border
+                </v-icon>
 
-	                <v-icon
-	                  v-else
-	                  color="yellow"
-	                >
-	                  star
-	                </v-icon>
-	              </v-list-item-action>
-	            </template>
-	          </v-list-item>
+                <v-icon
+                  v-else
+                  color="yellow"
+                >
+                  star
+                </v-icon>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
 
-	          <v-divider
-	            v-if="index + 1 < items.length"
-	            :key="index"
-	          ></v-divider>
-	        </template>
-	      </v-list-item-group>
-	    </v-list>
-	</v-card>
-</v-col>
-</v-row>
-<v-row>
-	<v-col>
-    <v-card
-        max-width="500"
-        class="mx-auto"
-        flat color="basil"
-    >
-	<v-toolbar
-	 	@click="showTitle"
-	      	:color="setThis === 0 ? 'purple lighten-3' : 'blue lighten-2'"
-	     	dark
-	    >
-
-	      <v-toolbar-title >{{titles[setThis]}}</v-toolbar-title>
-
-	      <div class="flex-grow-1"></div>
-
-	      <v-btn icon>
-	        <v-icon>mdi-magnify</v-icon>
-	      </v-btn>
-
-	      <v-btn icon>
-	        <v-icon>mdi-checkbox-marked-circle</v-icon>
-	      </v-btn>
-	    </v-toolbar>
-	    <v-list two-line>
-
-	      <v-list-item-group
-	        v-model="selected"
-	        multiple
-	        active-class="pink--text"
-	      >
-	        <template v-for="(item, index) in items">
-	          <v-list-item :key="item.title">
-	            <template v-slot:default="{ active, toggle }">
-	              <v-list-item-content>
-	                <v-list-item-title v-text="item.title"></v-list-item-title>
-	                <v-list-item-subtitle class="text--primary" v-text="item.headline"></v-list-item-subtitle>
-	                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
-	              </v-list-item-content>
-
-	              <v-list-item-action>
-	                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
-	                <v-icon
-	                  v-if="!active"
-	                  color="grey lighten-1"
-	                >
-	                  star_border
-	                </v-icon>
-
-	                <v-icon
-	                  v-else
-	                  color="yellow"
-	                >
-	                  star
-	                </v-icon>
-	              </v-list-item-action>
-	            </template>
-	          </v-list-item>
-
-	          <v-divider
-	            v-if="index + 1 < items.length"
-	            :key="index"
-	          ></v-divider>
-	        </template>
-	      </v-list-item-group>
-	    </v-list>
-	</v-card>
-</col>
-</row>
+          <v-divider
+            v-if="index + 1 < items.length"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 <script>
   export default {
