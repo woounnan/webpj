0fda90ee5e82a7051d409a2ffacc5795b7740ceb
woounnan <woounnan@gmail.com>
2019-10-09 10:40:03 +0900

    test reg works in store.js

--- front/src/store.js
+++ front/src/store.js
@@ -19,6 +19,7 @@ var regWork = (works, cv, to) => {
     //이미 등록이 되어있다
     //대상만 추가해준다.
     for(x in works){
+      console.log('date:::', works[x])
       if(works[x].date === cv.date){
         works[x].to.push(to)
         console.log('already registered work')
