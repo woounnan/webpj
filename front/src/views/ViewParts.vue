17bf24a24801367cca6f01f6695206cae2896f62
woounann <woounann@gmail.com>
2019-10-10 20:06:00 +0900

    test open viewwork using modal

--- front/src/views/ViewParts.vue
+++ front/src/views/ViewParts.vue
@@ -119,7 +119,7 @@
               <template v-slot:default="{ active, toggle }">
         <v-dialog v-model="dialog" width="600px">
          <template v-slot:activator="{ on:dial }">
-                <v-list-item-content v-on="dial">
+                <v-list-item-content v-on="dial"  @click="sendWork(item)">
 
                   <v-list-item-title>
 
