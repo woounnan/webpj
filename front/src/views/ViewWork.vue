68db8c1815d39ee2b79a7153ee3ea8de4d3eae63
woounnan <woounnan@gmail.com>
2019-10-10 22:59:46 +0900

    test multiple runs ViewWorks.vue

--- front/src/views/ViewWork.vue
+++ front/src/views/ViewWork.vue
@@ -103,7 +103,7 @@
           :key="item.title"
         >
         <v-icon>reply</v-icon>
-          <v-list-item-avatar height="10px">
+          <v-list-item-avatar align="center" justify="center">
             <v-img :src="item.avatar"></v-img>
           </v-list-item-avatar>
 
