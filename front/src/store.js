caeb662ebb25b21fff7730feae8624930601af51
woounnan <woounnan@gmail.com>
2019-10-12 23:26:33 +0900

    test ordered

--- front/src/store.js
+++ front/src/store.js
@@ -144,11 +144,12 @@ export default new Vuex.Store({
               else{
                 if(cv.works.by === state.user.id){
                   //요청작업 등록
-                  console
+                  console.log('11111111111')
                   regWork(state.user.works.toWork, cv, x.with)
                   //state.user.works.toWork.splice(0, 1)
                 }else{
                   //받은작업 등록
+                  console.log('222222222222')
                   regWork(state.user.works.fromWork, cv, x.with)
                   //state.user.works.fromWork.splice(0, 1)
 
