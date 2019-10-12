479e5055cef1618d846b9070326e770e7640cafb
woounnan <woounnan@gmail.com>
2019-10-12 19:30:54 +0900

    test flag_expired

--- apis/index.js
+++ apis/index.js
@@ -14,23 +14,17 @@ var list_user = [{
 	id:'',
 	sock:undefined
 }]
-
-
-var saveMsg = function (to, from, newConvs){
-	//if 메시지가 work라면
-	//expired callback을 걸어준다.
-	if(newConvs.works.notice != undefined){
-		console.log('this is works!!!')
-	}
+const msgPush = (to, from, newConvs)=>{
 	User.findOne({position: from}, (e, r) => {
 		if(e){
-			console.error('getUser Error in index.js:::: ', e)
+			console.error('findOne Error in index.js:::: ', e)
 		}
 		var flag = 0
+		var idx = -1
 		for(x in r.comu){
 			if(r.comu[x].with === to){
 				console.log('find idx ::')
-				var idx = x
+				idx = x
 				flag = 1
 				break
 			}
@@ -49,7 +43,7 @@ var saveMsg = function (to, from, newConvs){
 			},
 			(e, r) => {
 				if(e)
-					console.error('getUser--non exist Error:::::', e)
+					console.error('findOne--non exist Error:::::', e)
 			})
 		}
 		else{
@@ -63,12 +57,64 @@ var saveMsg = function (to, from, newConvs){
 			},
 			(e, r) => {
 				if(e)
-					console.error('getUser--exist  Error:::::', e)
+					console.error('findOne--exist  Error:::::', e)
 			})
 		}
-		
 	})
+}
 
+const msgSet = (to, from, newConvs)=>{
+	User.findOne({position: from}, (e, r) => {
+		if(e){
+			console.error('findOne Error in index.js:::: ', e)
+		}
+		var flag = 0
+		var idx = -1
+		for(x in r.comu){
+			if(r.comu[x].with === to){
+				console.log('find idx ::')
+				idx = x
+				flag = 1
+				break
+			}
+		}
+		r.comu[idx].convs.forEach(x => {
+			//해당 work 메시지를 검색
+			if(x.date == newConvs.date){
+				console.log('work 찾음!!!::::', JSON.stringify(x))
+				x.works.flag_expired = true
+			}
+		})
+		//바꾼 값으로 update
+		User.update({
+				position : from
+			}, {
+			$set : {
+				comu: r.comu
+			} 
+		},
+		(e, r) => {
+			if(e)
+				console.error('findOne--exist  Error:::::', e)
+		})
+	})
+}
+
+var saveMsg = function (to, from, newConvs){
+	//if 메시지가 work라면
+	//expired callback을 걸어준다.
+	if((newConvs.works.notice != undefined) && (newConvs.works.flag_expired == false)){
+		console.log('this is works and deadline has not yet passed!!!')
+		console.log('새로 works 가 등록되었을 때  flag_expired 값은::::', newConvs.works.flag_expired)
+		setTimeout((works)=>{
+	    	console.log('diffSec::::', diffSec(newConvs.endDate))
+	    	newConvs.works.flag_expired = true
+	    	//update db to set flag_expired value on true
+	    	msgSet(to, from, newConvs)
+		}, 1000 * 30, works)
+	}
+	//그 후 메시지를 저장한다.
+	msgPush(to, from, newConvs)
 }
 
 
