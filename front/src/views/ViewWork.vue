dde00b177a64d63202e1e1d06ec4d01b17dac486
woounnan <woounnan@gmail.com>
2019-10-13 13:50:04 +0900

    test

--- front/src/views/ViewWork.vue
+++ front/src/views/ViewWork.vue
@@ -51,22 +51,30 @@
           </v-list-item>
           <v-divider inset></v-divider>            
           <v-list-item>
+            <v-row >
+              <v-col cols="2">
             <v-list-item-action>
               <v-icon>date_range</v-icon>
             </v-list-item-action>
-            <v-list-item-content>
+
+                <v-col cols="4">
                 <v-text-field 
                 readonly
               >{{works.convs.works.startDate}}~{{works.convs.works.endDate}}</v-text-field>
+            </v-col>
+            <v-col cols="2">
             <v-list-item-action>
               <v-icon>system_update_alt</v-icon>
             </v-list-item-action>
+          </v-col>
+            <v-col cols="4">
                 <v-text-field 
                 v-model="items.startDate"
                 readonly
               />
+            </v-col>
+            </v-row>
 
-            </v-list-item-content>
           </v-list-item>
           <v-divider inset></v-divider>
         <v-list three-line
