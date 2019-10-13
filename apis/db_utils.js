091b04fbf4c37aecffb72e463db688847af44243
woounnan <woounnan@gmail.com>
2019-10-13 14:00:49 +0900

    test

--- apis/db_utils.js
+++ apis/db_utils.js
@@ -50,10 +50,8 @@ router.post('/work', upload.single('bin'), function (req, res, next) {
 		by : req.body.by,
 	}
 	if(req.body.flag_uploadFile == 'true'){
-		works['file_s_real'] = req.file.originalfilename
+		works['file_s_real'] = req.file.originalname
 		works['file_s_save'] = req.file.filename
-		console.log('work save in db_utils::::', JSON.stringify(req.file))
-		console.log('work real in db_utils::::', works.file_s_real)
 	}
 	else
 		console.log('not uploaded files')
