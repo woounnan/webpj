9e53a34d60a9cfa62bfa164cc1c9cab7665bd31b
woounnan <woounnan@gmail.com>
2019-10-13 11:13:45 +0900

    adjust button in ViewWork.vue

--- front/src/store.js
+++ front/src/store.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import io from 'socket.io-client'
 Vue.use(Vuex)
 var getAvatar = (state, position)=>{
+  console.log('getOthers in getAvatar::::', state.getters.getOthers)
   state.getters.getOthers.forEach(x => {
     if(x.position === position){
       console.log('getAvatar:::find it !!!! :::')
