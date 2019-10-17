75f6695264526bb217b280d081ade41be0de29db
woounnan <woounnan@gmail.com>
2019-10-17 20:33:15 +0900

    tesT

--- apis/index.js
+++ apis/index.js
@@ -77,7 +77,7 @@ const msgSet = (to, from, newConvs, fieldName, value)=>{
 		var flag = 0
 		var idx = -1
 		var findDate = newConvs.works.flag_date != newConvs.date ? newConvs.works.flag_date : newConvs.date
-
+		console.log('findDate:::', findDate)
 		for(x in r.comu){
 			if(r.comu[x].with === to){
 				console.log('find idx ::', r.comu[x].with)
