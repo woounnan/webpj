a7f873cab3633b9926d47c29bd1f95df0692add6
woounnan <woounnan@gmail.com>
2019-10-17 20:54:11 +0900

    tesT

--- apis/index.js
+++ apis/index.js
@@ -92,7 +92,7 @@ const msgSet = (to, from, newConvs, fieldName, value)=>{
 
 			if((x.date == findDate) && (x.date == x.works.flag_date)){
 				//x.works['fieldName'] = value
-				Object.assign(x.works, {fieldName: value});
+				Object.assign(x.works, {'test': 123123123123});
 				console.log('x.date:::', x.date)
 
 				console.log('work 찾음!!!::::', r.comu[idx].convs[i].works)
