9008dbc3d86214542cb2a2874c8da816472d9fbb
woounnan <woounnan@gmail.com>
2019-10-18 12:28:36 +0900

    test

--- apis/index.js
+++ apis/index.js
@@ -87,13 +87,7 @@ const msgSet = (to, from, newConvs, fieldName, value, test)=>{
 			}
 		}
 		var i =0
-		console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
-		r.comu[idx].convs.forEach(x => {
-			//해당 work 메시지를 검색				
-			console.log(x)
-		})
 
-		console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
 
 		r.comu[idx].convs.forEach(x => {
 			//해당 work 메시지를 검색				
