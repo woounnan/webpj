d36c76a878092b3aa5a1b397c851eb6d8e277011
woounnan <woounnan@gmail.com>
2019-10-20 12:42:01 +0900

    test

--- front/src/views/ViewWork.vue
+++ front/src/views/ViewWork.vue
@@ -111,33 +111,33 @@
             ></v-divider>
 
             <v-list-item
-              :key="item.position"
+              :key="jobs.to[index].position"
             >
                 <v-list-item-avatar>
-                <v-img :src="item.avatar"></v-img>
+                <v-img :src="jobs.to[index].avatar"></v-img>
                 </v-list-item-avatar>
 
                 <v-list-item-content>
-                <v-list-item-title v-html="item.position"></v-list-item-title>
+                <v-list-item-title v-html="jobs.to[index].position"></v-list-item-title>
                 <!--
-                <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                <v-list-item-subtitle v-html="jobs.to[index].subtitle"></v-list-item-subtitle>
                 -->
                 </v-list-item-content>
 
                 <template v-if="jobs.convs.works.by === $store.getters.getUser.id">
                   <v-list-item-content @click="showState(item)">
-                    <v-btn v-if="item.state === '미제출'" depressed small color="error">미제출</v-btn>
-                    <v-btn v-if="item.state === '승인대기'" depressed small color="primary">승인대기</v-btn>
-                    <v-btn v-if="item.state === '승인거절'" depressed small color="orange darken-3 white--text">승인거절</v-btn>
-                    <v-btn v-if="item.state === '승인완료'" depressed small color="green darken-1 white--text">승인완료</v-btn>
+                    <v-btn v-if="jobs.to[index].state === '미제출'" depressed small color="error">미제출</v-btn>
+                    <v-btn v-if="jobs.to[index].state === '승인대기'" depressed small color="primary">승인대기</v-btn>
+                    <v-btn v-if="jobs.to[index].state === '승인거절'" depressed small color="orange darken-3 white--text">승인거절</v-btn>
+                    <v-btn v-if="jobs.to[index].state === '승인완료'" depressed small color="green darken-1 white--text">승인완료</v-btn>
                  </v-list-item-content>
                   <v-list-item-content>
-                      <v-icon :color="item.flag_upload ? 'deep-purple accent-4' : 'grey'">insert_drive_file</v-icon>
+                      <v-icon :color="jobs.to[index].flag_upload ? 'deep-purple accent-4' : 'grey'">insert_drive_file</v-icon>
                   </v-list-item-content>
                   <v-list-item-content>
-                      <v-list-item-subtitle >{{item.flag_sendDate}}</v-list-item-subtitle>
+                      <v-list-item-subtitle >{{jobs.to[index].flag_sendDate}}</v-list-item-subtitle>
                   </v-list-item-content>
-                    <div v-if="item.state === '승인대기'">
+                    <div v-if="jobs.to[index].state === '승인대기'">
                         <v-btn class="mx-2" fab dark small color="indigo" @click="sendWorkToS('승인완료')">
                           <v-icon dark>done</v-icon>
                         </v-btn>
@@ -153,7 +153,7 @@
                             <v-divider></v-divider>
                                 <v-textarea 
                                     placeholder="내용 입력"
-                                    v-model="item.comment"
+                                    v-model="jobs.to[index].comment"
                                     counter
                                     maxlength="120"
                                     full-width
@@ -171,7 +171,7 @@
                 </template>
                 <template v-else>
                   <!-- 미제출시 -->
-                  <div v-if="item.state==='미제출'">
+                  <div v-if="jobs.to[index].state==='미제출'">
                     <v-list-item-content>
                       <v-file-input
                         v-model="upFiles"
@@ -203,19 +203,19 @@
                         
                     </v-list-item-content>
                   <v-list-item-content>
-                      <v-list-item-subtitle >{{item.flag_sendDate}}</v-list-item-subtitle>
+                      <v-list-item-subtitle >{{jobs.to[index].flag_sendDate}}</v-list-item-subtitle>
                   </v-list-item-content>
                   </div>
                   <div v-else>
                     <v-list-item-content>
-                        <v-icon :color="item.flag_upload ? 'deep-purple accent-4' : 'grey'">insert_drive_file</v-icon>
+                        <v-icon :color="jobs.to[index].flag_upload ? 'deep-purple accent-4' : 'grey'">insert_drive_file</v-icon>
                     </v-list-item-content> 
                     <v-list-item-content>
-                      <v-list-item-subtitle >{{item.flag_sendDate}}</v-list-item-subtitle>
+                      <v-list-item-subtitle >{{jobs.to[index].flag_sendDate}}</v-list-item-subtitle>
                     </v-list-item-content>                                       
                   </div>
-                    <v-list-item-content v-if="item.state==='승인거절'">
-                      <v-icon v-on="showComment" :color="item.comment ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
+                    <v-list-item-content v-if="jobs.to[index].state==='승인거절'">
+                      <v-icon v-on="showComment" :color="jobs.to[index].comment ? 'deep-purple accent-4' : 'grey'">chat_bubble</v-icon>
                     </v-list-item-content>              
                                
                 </template>
