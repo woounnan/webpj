c857c2568ab42b4f51785de5f5a66280dbcf13f9
woounnan <woounnan@gmail.com>
2019-10-20 19:49:22 +0900

    fix ViewParts convs error

--- apis/index.js
+++ apis/index.js
@@ -147,6 +147,7 @@ var saveMsg = function (to, from, newConvs){
 		    	newConvs.works.flag_expired = true
 		    	//update db to set flag_expired value on true
 		    	msgSet(to, from, newConvs, 'flag_expired', true)
+		    	socket.broadcast('sock_initWorks')
 			}, 1000 * 30, newConvs)
 		}
 		else{
